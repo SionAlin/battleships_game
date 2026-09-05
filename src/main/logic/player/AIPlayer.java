e5020fcb10c5a92669a091c6c92922a6031ee10c
SionAlin <alin.sion878@gmail.com>
2026-09-05 16:37:22 +0300

    Human Player

--- src/main/logic/player/AIPlayer.java
+++ src/main/logic/player/AIPlayer.java
@@ -2,12 +2,35 @@ package logic.player;
 
 import model.*;
 import java.awt.Point;
+import java.util.Arrays;
 
 public class AIPlayer extends Player{
-
+    
+    private ArrayList<Point> BoardCells;
+    private ArrayList<Point> NextPossibleHits;
+    
+    public AIPlayer(String PlayerName){
+        super(PlayerName);
+        this.BoardCells = this.ConvenientBoardCells();
+        this.NextPossibleHits = new ArrayList<Point>();
+    }
+    
     @Override
     public void makeMove(Board opponentBoard){
         
+    }
+
+    private ArrayList<Point> ConvenientBoardCells(){
+        ArrayList<Point> list = new ArrayList<Point>();
+
+        for(int i = 0; i < 9; i += 2){
+            for(int j = 0; j < 9; j += 2){
+                list.add(Point(i,j));
+                list.add(Point(i+1, j+1));
+            }
+        }
+
+        return list;
     }
 
 }
